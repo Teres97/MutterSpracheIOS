1f8ada4efb3e4e1dad299def1903f920e43ff22a
Teres97 <teres97@mail.ru>
2023-05-22 17:59:34 +0200

    Initial Commit

---
+++ tSpeech/tSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  tSpeechApp.swift
+//  tSpeech
+//
+//  Created by Eduard Tereshchuk on 22.05.23.
+//
+
+import SwiftUI
+
+@main
+struct tSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
