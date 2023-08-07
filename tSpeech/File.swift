37c48e97f61c849411c0b1b7a420a25f0bf644c9
Teres97 <teres97@mail.ru>
2023-08-07 17:14:22 +0200

    alpha version MutterSpracheIOS

---
+++ tSpeech/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  tSpeech
+//
+//  Created by Eduard Tereshchuk on 22.05.23.
+//
+
+import Foundation
---
+++ tSpeech/sqlite3.swift
@@ -0,0 +1,8 @@
+//
+//  sqlite3.swift
+//  tSpeech
+//
+//  Created by Eduard Tereshchuk on 26.05.23.
+//
+
+import Foundation
